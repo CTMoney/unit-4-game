85b054d02301398b4c70d54da2b8d32d60c2c36b
Cat <money1062@live.com>
2018-12-17 16:35:47 -0500

    Working ver

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,95 @@
 var wins = 0, losses = 0;
 
-$(document).ready(function start() {
-    var desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1), 
-        crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
-        $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
-        $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
+var desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+    crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
 
-    $("#crystal1").click(function (){
-        currentScore += crystal1Value;        
+$(document).ready(function start() {
+
+    $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+    $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
+
+    $("#crystal1").click(function () {
+        currentScore += crystal1Value;
         $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         if (currentScore == desiredScore) {
             wins++;
             $("#winsHTML").html(`Wins: ${wins}`);
-            currentScore = 0;
-            start();
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
-        else if ( currentScore > desiredScore) {
+        else if (currentScore > desiredScore) {
             losses++;
             $("#lossesHTML").html(`Losses: ${losses}`);
-            currentScore = 0;
-            start();
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
     })
 
-    $("#crystal2").click(function (){
+    $("#crystal2").click(function () {
         currentScore += crystal2Value;
         $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         if (currentScore == desiredScore) {
             wins++;
             $("#winsHTML").html(`Wins: ${wins}`);
-            currentScore = 0;
-            start();
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
-        else if ( currentScore > desiredScore) {
+        else if (currentScore > desiredScore) {
             losses++;
-            currentScore = 0;
-            start();
+            $("#lossesHTML").html(`Losses: ${losses}`);
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
     })
 
-    $("#crystal3").click(function (){
+    $("#crystal3").click(function () {
         currentScore += crystal3Value;
         $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         if (currentScore == desiredScore) {
             wins++;
             $("#winsHTML").html(`Wins: ${wins}`);
-            currentScore = 0;
-            start();
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
-        else if ( currentScore > desiredScore) {
+        else if (currentScore > desiredScore) {
             losses++;
-            currentScore = 0;
-            start();
+            $("#lossesHTML").html(`Losses: ${losses}`);
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
     })
 
-    $("#crystal4").click(function (){
+    $("#crystal4").click(function () {
         currentScore += crystal4Value;
         $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         if (currentScore == desiredScore) {
             wins++;
             $("#winsHTML").html(`Wins: ${wins}`);
-            currentScore = 0;
-            start();            
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
-        else if ( currentScore > desiredScore) {
+        else if (currentScore > desiredScore) {
             losses++;
-            currentScore = 0;
-            start();
+            $("#lossesHTML").html(`Losses: ${losses}`);
+            desiredScore = (Math.floor(Math.random() * 100) + 1), crystal1Value = (Math.floor(Math.random() * 10) + 1), crystal2Value = (Math.floor(Math.random() * 10) + 1),
+                crystal3Value = (Math.floor(Math.random() * 10) + 1), crystal4Value = (Math.floor(Math.random() * 10) + 1), currentScore = 0;
+            $("#desiredScoreHTML").html(`Desired Score: ${desiredScore}`);
+            $("#currentScoreHTML").html(`Current Score: ${currentScore}`);
         }
     })
-
 
 });
